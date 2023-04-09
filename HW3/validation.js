57fb8484974921b4ee6f20c0efed1bef56dc24e4
JRSiwiecki <JRSiwiecki@gmail.com>
2023-04-08 18:48:11 -0700

    Finish front-end validation

--- HW3/validation.js
+++ HW3/validation.js
@@ -48,9 +48,13 @@ function checkSimilarity() {
     
     If any condition is not met, then we alert the user
     to which field has not been completed.
+
+    Otherwise, we let them know the form is valid.
 */
 function validateForm() {
   let alertString = "The following input fields must be completed:\n";
+
+  // check text inputs
   let valid = true;
   let currentTextInput = "";
 
@@ -63,8 +67,70 @@ function validateForm() {
     }
   }
 
+  // check first set of radio buttons
+  let animalRadioIsSet = false;
+
+  for (let i = 0; i < animalRadios.length; i++) {
+    currentAnimalRadio = animalRadios[i].checked;
+
+    if (currentAnimalRadio) {
+      animalRadioIsSet = true;
+      break;
+    }
+  }
+
+  if (!animalRadioIsSet) {
+    valid = false;
+    alertString += "\tRadio Input Set 1\n";
+  }
+
+  // check second set of radio buttons
+  // this is probably a lazy copy and paste but what if
+  // the client wants us to add more radio buttons!!!
+  let gameRadioIsSet = false;
+  for (let i = 0; i < gameRadios.length; i++) {
+    currentGameRadio = gameRadios[i].checked;
+
+    if (currentGameRadio) {
+      gameRadioIsSet = true;
+      break;
+    }
+  }
+
+  if (!gameRadioIsSet) {
+    valid = false;
+    alertString += "\tRadio Input Set 2\n";
+  }
+
+  // check checkboxes
+
+  let languagesBoxIsSet = false;
+  for (let i = 0; i < languageBoxes.length; i++) {
+    currentLanguageBox = languageBoxes[i].checked;
+
+    if (currentLanguageBox) {
+      languagesBoxIsSet = true;
+      break;
+    }
+  }
+
+  if (!languagesBoxIsSet) {
+    valid = false;
+    alertString += "\tCheckbox Input Set 1\n";
+  }
+
+  // don't need to check class dropdown because we set a default option
+
+  // check if text area has input
+  if (textAreaInput.value.length == 0) {
+    valid = false;
+    alertString += "\tText Area Input\n";
+  }
+
   if (!valid) {
     alert(alertString);
+  } else {
+    alert("Form is valid and ready to be submitted!");
   }
 }
 
