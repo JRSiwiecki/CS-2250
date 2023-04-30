6a0389c4227fa141ecdfb3a84fa514e8aca8925f
JRSiwiecki <JRSiwiecki@gmail.com>
2023-04-29 22:19:04 -0700

    Initial HW5

---
+++ HW5/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Names from "./component/Names";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Names />
+        <Names />
+        <Names />
+      </header>
+    </div>
+  );
+}
+
+export default App;
