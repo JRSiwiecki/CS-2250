4abd2fd367774bc24c8cc47f6a5a707a4aa24d5e
JRSiwiecki <JRSiwiecki@gmail.com>
2023-05-01 17:56:31 -0700

    Add song call API

--- HW5/hw5/src/App.js
+++ HW5/hw5/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import ACVillager from "./components/ACVillager";
+import ACMusic from "./components/ACMusic";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <ACVillager />
+      <ACMusic />
     </div>
   );
 };
---
+++ HW5/hw5/src/components/ACMusic.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import axios from "axios";
+
+function ACMusic()
+{
+    const [songUrl, setCurrentSong] = useState();
+    
+    // Song IDs in Animal Crossing are 1 - 72
+    function getCurrentHour()
+    {
+        const date = new Date();
+        return date.getHours();
+    }
+    
+    // Make API call
+    function getBackgroundSong()
+    {
+        axios.get("https://acnhapi.com/v1/backgroundmusic")
+            .then(response => {
+                getCurrentSong(response.data, getCurrentHour())
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    // Get current song based on hour in real time
+    function getCurrentSong(songsJson, currentHour)
+    {
+        let songArray = [];
+        
+        for (let song in songsJson)
+        {
+            songArray.push([song, songsJson[song]]);
+        }
+
+        // Create songString to find the correct song in the songsJson
+        // If the currentHour is 10 or higher, just use the currentHour
+        // Otherwise, use the hour with a 0 in front of it.
+        let songString = "BGM_24Hour_" + 
+                        (currentHour >= 10 ? currentHour : "0" + currentHour) +
+                        "_Sunny";
+        
+        setCurrentSong(songsJson[songString].music_uri);
+    }
+
+    return (
+        <div className="ac-music">
+            <button className="ac-music-button" onClick={getBackgroundSong}>Play Current (Sunny) AC:NH Song</button>
+            
+            <br />
+        
+            <audio controls src={songUrl}></audio>
+        </div>
+    );
+
+}
+
+export default ACMusic;
